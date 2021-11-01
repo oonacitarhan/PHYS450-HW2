ec5284b0537b12a99710dee3bd94982f9aed5d02
onacitarhan17 <onacitarhan17@ku.edu.tr>
2021-11-01 16:11:27 +0300

    q2 is done???

--- solutions.py
+++ solutions.py
@@ -18,8 +18,26 @@ def q1():
                 l1_norm += np.abs(dm_psi_arr[i][j])
     print("l1-norm of coherence: ", l1_norm)
 
+def q2():
+    lambda1 = (1+1.0j)/sqrt(3)
+    lambda2 = 1/sqrt(3)
+    psi = lambda1*basis(4,0) + lambda2*basis(4,3)
+    dm_psi = ket2dm(psi)
+    dm_psi_arr = dm_psi.full(order='C')
+    pt_dm = (dm_psi_arr[0][0]+dm_psi_arr[1][1]) * ket2dm(basis(2, 0)) + (dm_psi_arr[2][2]+dm_psi_arr[3][3]) * ket2dm(basis(2, 1))
+    print("q2")
+    print("Tot. Corr. shared in psi: ",2*entropy_vn(pt_dm) - entropy_vn(dm_psi))
+    dm_psi_diag = dm_psi_arr[3][3] * ket2dm(basis(4, 3))
+    for i in range(3):
+        dm_psi_diag += dm_psi_arr[i][i] * ket2dm(basis(4, i))
+    dm_psi_diag_arr = dm_psi_diag.full(order='C')
+    pt_dm_diag = (dm_psi_diag_arr[0][0]+dm_psi_diag_arr[1][1]) * ket2dm(basis(2, 0)) + (dm_psi_diag_arr[2][2]+dm_psi_diag_arr[3][3]) * ket2dm(basis(2, 1))
+    print("Tot. Corr. shared in qd: ",2*entropy_vn(pt_dm_diag) - entropy_vn(dm_psi_diag))
+
 def main():
     print("")
     q1()
+    print("")
+    q2()
 
 main()
